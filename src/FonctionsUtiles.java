9365822ae7d912a5020367e8da539c08fd2766a5
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-11-13 18:05:27 +0100

    Ajout des fonctions alea

---
+++ src/FonctionsUtiles.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class FonctionsUtiles {
+    public static double loiExponentielle(double lambda) {
+        return -Math.log(Math.random()) / lambda;
+    }
+
+    public static double loiUniforme(double a, double b) {
+        return a + (b - a) * Math.random();
+    }
+}
