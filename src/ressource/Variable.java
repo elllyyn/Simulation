748f60023a4033e9dcca46762c83b6478e725feb
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-11-14 12:17:33 +0100

    Ajout de fonction pour initialiser les variables et ajout de constantes

--- src/ressource/Variable.java
+++ src/ressource/Variable.java
@@ -2,6 +2,7 @@ package ressource;
 
 import modele.Bus;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Variable {
@@ -11,4 +12,20 @@ public class Variable {
     public static int nbReparation;
     public static List<Bus> queueControle;
     public static List<Bus> queueReparation;
+
+    public static final byte lambdaArriveeBus = 45;
+    public static final int tempsMinControle = 15;
+    public static final int tempsMaxControle = 65;
+    public static final int tempsMinReparation = 168;
+    public static final int tempsMaxReparation = 330;
+    public static final int tempsSimulation = 160 * 60;
+
+    public static void initialiser() {
+        statusControle = false;
+        statusReparartion = 0;
+        nbBus = 0;
+        nbReparation = 0;
+        queueControle = new ArrayList<>();
+        queueReparation = new ArrayList<>();
+    }
 }
