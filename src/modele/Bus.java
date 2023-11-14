869ed893191e9da0b7acddc5df2b43b11ee16f7d
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-11-14 15:15:08 +0100

    Modification de bus avec constructeur en plus

--- src/modele/Bus.java
+++ src/modele/Bus.java
@@ -1,5 +1,11 @@
 package modele;
 
+import utils.FonctionsUtiles;
+
 public class Bus {
     public boolean possedeDefaut;
+
+    public Bus() {
+        possedeDefaut = FonctionsUtiles.doitReparer();
+    }
 }
