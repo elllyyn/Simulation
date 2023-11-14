4d42c8fd58c75a2b60e94ae6e923ea5e0804771f
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-11-14 15:07:49 +0100

    Ajout des mises à jour des aires

--- src/evenement/MiseAJourDesAires.java
+++ src/evenement/MiseAJourDesAires.java
@@ -1,5 +1,18 @@
 package evenement;
 
-public class MiseAJourDesAires {
-    
+import ressource.Variable;
+import statistique.IndicateurStatistique;
+
+public class MiseAJourDesAires implements Evenement{
+    @Override public void lancerEvenement() {
+        // R
+    }
+
+    public MiseAJourDesAires(int h1, int h2){
+        IndicateurStatistique.aireQueueControle = IndicateurStatistique.aireQueueControle + (h2 - h1) * Variable.queueControle.size();
+        IndicateurStatistique.aireQueueReparation = IndicateurStatistique.aireQueueReparation + (h2 - h1) * Variable.queueReparation.size();
+        IndicateurStatistique.tauxUtilisationReparation = IndicateurStatistique.tauxUtilisationReparation + (h2 - h1) * Variable.statusReparartion;
+    }
+
+
 }
--- src/statistique/IndicateurStatistique.java
+++ src/statistique/IndicateurStatistique.java
@@ -7,13 +7,8 @@ public class IndicateurStatistique {
     public static double tpsAttenteMoyFileControle;
     public static double tpsAttenteMoyFileReparation;
     public static double tauxUtilisationReparation;
-    private static double aireQueueControle = 0;
-    private static double aireQueueReparation = 0;
-
-    public void majAire(int h1, int h2){
-        aireQueueControle+=(h2-h1)*aireQueueControle;
-
-    }
+    public static double aireQueueControle = 0;
+    public static double aireQueueReparation = 0;
 
     public void calculerStatistiques(){
         tpsAttenteMoyFileControle = 1/Variable.nbBus * aireQueueControle;
