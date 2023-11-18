e74fc389632a7c8c3bd472759291ada0a25ee7d0
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-11-18 13:39:11 +0100

    Fonction pour maj les ind et maj de l'affichage

--- src/evenement/FinSimulation.java
+++ src/evenement/FinSimulation.java
@@ -3,16 +3,12 @@ package evenement;
 import entite.Bus;
 import simulateur.Simulateur;
 import statistique.IndicateurStatistique;
-import utils.Constantes;
 
 public class FinSimulation extends Evenement {
     @Override
     public void lancerEvenement(Simulateur simulateur) {
         simulateur.getEcheancier().getEvenements().clear();
-
-        IndicateurStatistique.setTpsAttenteMoyFileControle(IndicateurStatistique.getAireQueueControle() / simulateur.getNbBus());
-        IndicateurStatistique.setTpsAttenteMoyFileReparation(IndicateurStatistique.getAireQueueReparation() / simulateur.getNbReparation());
-        IndicateurStatistique.setTauxUtilisationReparation(IndicateurStatistique.getAirePosteReparation() / (2 * Constantes.dureeSimulation));
+        IndicateurStatistique.majIndicateurs(simulateur);
     }
 
     public FinSimulation(double temps, Bus bus) {
--- src/statistique/IndicateurStatistique.java
+++ src/statistique/IndicateurStatistique.java
@@ -1,11 +1,14 @@
 package statistique;
 
 import simulateur.Simulateur;
+import utils.Constantes;
 
 public class IndicateurStatistique {
     private static double tpsAttenteMoyFileControle = 0;
     private static double tpsAttenteMoyFileReparation = 0;
     private static double tauxUtilisationReparation = 0;
+    private static double tempsMaxAttControle = 0;
+    private static double tempsMaxAttReparation = 0;
     private static double aireQueueControle = 0;
     private static double aireQueueReparation = 0;
     private static double airePosteReparation = 0;
@@ -58,19 +61,45 @@ public class IndicateurStatistique {
         IndicateurStatistique.airePosteReparation = airePosteReparation;
     }
 
+    public static double getTempsMaxAttControle() {
+        return tempsMaxAttControle;
+    }
+
+    public static void setTempsMaxAttControle(double tempsMaxAttControle) {
+        IndicateurStatistique.tempsMaxAttControle = tempsMaxAttControle;
+    }
+
+    public static double getTempsMaxAttReparation() {
+        return tempsMaxAttReparation;
+    }
+
+    public static void setTempsMaxAttReparation(double tempsMaxAttReparation) {
+        IndicateurStatistique.tempsMaxAttReparation = tempsMaxAttReparation;
+    }
+
+    public static void majIndicateurs(Simulateur simulateur)
+    {
+        setTpsAttenteMoyFileControle(getAireQueueControle() / simulateur.getNbBus());
+        setTpsAttenteMoyFileReparation(getAireQueueReparation() / simulateur.getNbReparation());
+        setTauxUtilisationReparation(getAirePosteReparation() / (2 * Constantes.dureeSimulation));
+    }
+
     public static void printStats(Simulateur simulateur)
     {
         System.out.println("\n----------STATS GLOBALES----------");
-        System.out.println("Temps d'attente moyen dans la file de controle : " + tpsAttenteMoyFileControle);
-        System.out.println("Temps d'attente moyen dans la file de reparation : " + tpsAttenteMoyFileReparation);
+        System.out.println("Temps d'attente moyen dans la file de controle sans les bus toujours en attente: " + (tpsAttenteMoyFileControle - (double)simulateur.getTempsToujoursFileCont()));
+        System.out.println("Temps d'attente moyen dans la file de reparation sans les bus toujours en attente: " + (tpsAttenteMoyFileReparation - (double)simulateur.getTempsToujoursFileRep()));
+        System.out.println("Temps d'attente moyen dans la file de controle avec les bus toujours en attente: " + tpsAttenteMoyFileControle);
+        System.out.println("Temps d'attente moyen dans la file de reparation avec les bus toujours en attente: " + tpsAttenteMoyFileReparation);
         System.out.println("Taux d'utilisation du poste de reparation : " + tauxUtilisationReparation);
+        System.out.println("Nombre de bus en attente : " + (simulateur.getListeBusFileC().size() + simulateur.getListeBusFileR().size()));
         System.out.println("Nombre de bus total : " + simulateur.getNbBus());
         System.out.println("Taux de bus reparer : " + ((double)simulateur.getNbReparation() / (double)simulateur.getNbBus()) * 100 + "%");
     }
 
     public static void printTempsMaxContAndRep(Simulateur simulateur){
         System.out.println("\n----------STATS BUS----------");
-        System.out.println("Temps max d'attente dans la file de controle : " + simulateur.getTempsMaxAttControle());
-        System.out.println("Temps max d'attente dans la file de reparation : " + simulateur.getTempsMaxAttRep());
+        System.out.println("Temps max d'attente dans la file de controle : " + IndicateurStatistique.getTempsMaxAttControle());
+        System.out.println("Temps max d'attente dans la file de reparation : " + IndicateurStatistique.getTempsMaxAttReparation());
     }
 }
